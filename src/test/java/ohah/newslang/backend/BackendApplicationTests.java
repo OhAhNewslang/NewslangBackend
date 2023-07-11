7f271be78175f9cfc61336f8dca5ece83472fba9
oh jinseok <ojs258@naver.com>
2023-07-11 11:59:31 +0900

    project settings

---
+++ src/test/java/ohah/newslang/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ohah.newslang.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
