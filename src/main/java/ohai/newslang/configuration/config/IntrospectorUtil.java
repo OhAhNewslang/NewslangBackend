c4a8932c9fceb9f83291a7832f36a140b5771ab4
gongtaelee <dnjsxo0306@gmail.com>
2023-10-02 20:50:56 +0900

    시큐리티 관련 코드 주석 복구

--- src/main/java/ohai/newslang/configuration/config/IntrospectorUtil.java
+++ src/main/java/ohai/newslang/configuration/config/IntrospectorUtil.java
@@ -1,17 +1,17 @@
-//package ohai.newslang.configuration.config;
-//
-//import org.springframework.context.ApplicationContext;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
-//
-//public class IntrospectorUtil {
-//    private static final String HANDLER_MAPPING_INTROSPECTOR_BEAN_NAME = "mvcHandlerMappingIntrospector";
-//
-//    private IntrospectorUtil() {
-//    }
-//
-//    public static HandlerMappingIntrospector getIntrospector(HttpSecurity http) {
-//        ApplicationContext context = http.getSharedObject(ApplicationContext.class);
-//        return context.getBean(HANDLER_MAPPING_INTROSPECTOR_BEAN_NAME, HandlerMappingIntrospector.class);
-//    }
-//}
+package ohai.newslang.configuration.config;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+public class IntrospectorUtil {
+    private static final String HANDLER_MAPPING_INTROSPECTOR_BEAN_NAME = "mvcHandlerMappingIntrospector";
+
+    private IntrospectorUtil() {
+    }
+
+    public static HandlerMappingIntrospector getIntrospector(HttpSecurity http) {
+        ApplicationContext context = http.getSharedObject(ApplicationContext.class);
+        return context.getBean(HANDLER_MAPPING_INTROSPECTOR_BEAN_NAME, HandlerMappingIntrospector.class);
+    }
+}
--- src/main/java/ohai/newslang/configuration/jwt/TokenDecoder.java
+++ src/main/java/ohai/newslang/configuration/jwt/TokenDecoder.java
@@ -1,23 +1,23 @@
-//package ohai.newslang.configuration.jwt;
-//
-//import jakarta.servlet.http.HttpServletRequest;
-//import ohai.newslang.domain.entity.member.Member;
-//import org.springframework.security.core.Authentication;
-//
-//
-//public interface TokenDecoder {
-//    void init();
-//    // ids -> userId, hotelId
-//    void createToken(String role, String... ids);
-//    Authentication getAuthentication(String token);
-//    // 추출한 토큰에서 memberId 가져오기
-//    Long tokenToId(String token);
-//    // 추출한 토큰에서 role 가져오기
-//    String tokenToRole(String token);
-//    // 토큰 추출(우리의 경우 세션에서)
-//    String resolveToken(HttpServletRequest req);
-//    // 토큰 유효기간 검사
-//    boolean expiredToken(String token);
-//    // 현재 로그인한 사용자 정보 가져오기
-//    Long currentUserId();
-//}
+package ohai.newslang.configuration.jwt;
+
+import jakarta.servlet.http.HttpServletRequest;
+import ohai.newslang.domain.entity.member.Member;
+import org.springframework.security.core.Authentication;
+
+
+public interface TokenDecoder {
+    void init();
+    // ids -> userId, hotelId
+    void createToken(String role, String... ids);
+    Authentication getAuthentication(String token);
+    // 추출한 토큰에서 memberId 가져오기
+    Long tokenToId(String token);
+    // 추출한 토큰에서 role 가져오기
+    String tokenToRole(String token);
+    // 토큰 추출(우리의 경우 세션에서)
+    String resolveToken(HttpServletRequest req);
+    // 토큰 유효기간 검사
+    boolean expiredToken(String token);
+    // 현재 로그인한 사용자 정보 가져오기
+    Long currentUserId();
+}
