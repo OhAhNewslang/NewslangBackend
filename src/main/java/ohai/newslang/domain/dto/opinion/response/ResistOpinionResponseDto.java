9e8817b21308ae185d3891f87eb74311d5c7cf07
gonggongtae <dnjsxo0306@gmail.com>
2023-10-30 16:27:41 +0900

    의견 등록 api 수정

--- src/main/java/ohai/newslang/domain/dto/opinion/response/ResistOpinionResponseDto.java
+++ src/main/java/ohai/newslang/domain/dto/opinion/response/ResistOpinionResponseDto.java
@@ -11,21 +11,11 @@ import java.time.LocalDate;
 @Data
 @NoArgsConstructor
 public class ResistOpinionResponseDto {
-    private String opinionId;
-    private String memberName;
-    private LocalDate opinionCreateDate;
-    private String opinionContent;
-    private boolean modifiable;
-    private int likeCount;
+    private OpinionResponseDto opinion;
     private RequestResult result;
     @Builder
     public ResistOpinionResponseDto(Opinion opinion, boolean modifiable, RequestResult result) {
-        this.opinionId = opinion.getUuid();
-        this.memberName = opinion.getMember().getName();
-        this.opinionCreateDate = opinion.getCreateTime().toLocalDate();
-        this.opinionContent = opinion.getContent();
-        this.likeCount = opinion.getLikeCount();
-        this.modifiable = modifiable;
+        this.opinion = OpinionResponseDto.builder().opinion(opinion).modifiable(modifiable).build();
         this.result = result;
     }
 }
